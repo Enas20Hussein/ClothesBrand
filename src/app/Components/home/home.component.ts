fe2e650dfa27a853503886f39bc5e66c2d4023df
Enas <enashussein899@gmail.com>
2024-10-20 01:22:43 +0300

    Styling

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductComponent } from "../product/product.component";
 import { OurServicesComponent } from "../our-services/our-services.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ProductComponent, OurServicesComponent],
+  imports: [CommonModule, ProductComponent, OurServicesComponent,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
